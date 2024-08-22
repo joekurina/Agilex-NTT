640a5c229473c98afa0995a4b0dd77bd2bedf4e1
Joe K <142754495+joekurina@users.noreply.github.com>
2024-08-21 21:20:43 -0700

    fixed ntt.h and removed extern c linkage

--- include/kernel/ntt.h
+++ include/kernel/ntt.h
@@ -1,4 +1,3 @@
-// ntt.h
 #ifndef NTT_H
 #define NTT_H
 
@@ -11,25 +10,45 @@ using namespace cl::sycl;
 template <size_t idx>
 class FWD_NTT;
 
-// Declare the NTT functions
+// Declare the NTT functions with buffer-based signatures
 template <size_t id>
-void fwd_ntt_kernel(sycl::queue& q);
+void fwd_ntt_kernel(sycl::queue& q,
+                    buffer<uint64_t, 1>& inData_buf,
+                    buffer<uint64_t, 1>& inData2_buf,
+                    buffer<uint64_t, 1>& modulus_buf,
+                    buffer<uint64_t, 1>& twiddleFactors_buf,
+                    buffer<uint64_t, 1>& barrettTwiddleFactors_buf,
+                    buffer<uint64_t, 1>& outData_buf);
 
-void ntt_input_kernel(unsigned int numFrames, uint64_t* k_inData,
-                      uint64_t* k_inData2, uint64_t* k_modulus,
-                      uint64_t* k_twiddleFactors,
-                      uint64_t* k_barrettTwiddleFactors);
+void ntt_input_kernel(buffer<uint64_t, 1>& inData_buf,
+                      buffer<uint64_t, 1>& inData2_buf,
+                      buffer<uint64_t, 1>& modulus_buf,
+                      buffer<uint64_t, 1>& twiddleFactors_buf,
+                      buffer<uint64_t, 1>& barrettTwiddleFactors_buf,
+                      unsigned int numFrames,
+                      sycl::queue& q);
 
-void ntt_output_kernel(int numFrames, uint64_t* k_outData);
+void ntt_output_kernel(buffer<uint64_t, 1>& outData_buf,
+                       int numFrames,
+                       sycl::queue& q);
 
-// Declare the interface functions for external C linkage
-extern "C" {
-    void fwd_ntt(sycl::queue& q);
-    sycl::event ntt_input(sycl::queue& q, unsigned int numFrames, uint64_t* inData,
-                          uint64_t* inData2, uint64_t* modulus,
-                          uint64_t* twiddleFactors,
-                          uint64_t* barrettTwiddleFactors);
-    sycl::event ntt_output(sycl::queue& q, int numFrames, uint64_t* outData_in_svm);
-}
+// Declare the interface functions
+void fwd_ntt(sycl::queue& q,
+             buffer<uint64_t, 1>& inData_buf,
+             buffer<uint64_t, 1>& inData2_buf,
+             buffer<uint64_t, 1>& modulus_buf,
+             buffer<uint64_t, 1>& twiddleFactors_buf,
+             buffer<uint64_t, 1>& barrettTwiddleFactors_buf,
+             buffer<uint64_t, 1>& outData_buf);
+
+sycl::event ntt_input(sycl::queue& q, unsigned int numFrames,
+                      buffer<uint64_t, 1>& inData_buf,
+                      buffer<uint64_t, 1>& inData2_buf,
+                      buffer<uint64_t, 1>& modulus_buf,
+                      buffer<uint64_t, 1>& twiddleFactors_buf,
+                      buffer<uint64_t, 1>& barrettTwiddleFactors_buf);
+
+sycl::event ntt_output(sycl::queue& q, int numFrames,
+                       buffer<uint64_t, 1>& outData_buf);
 
 #endif // NTT_H
